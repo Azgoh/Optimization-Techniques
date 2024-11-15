21f03207e415e372ccfa0111041b5553144ed0cf
NikolasTsm <ntsamitr@ece.auth.gr>
2024-11-15 18:17:19 +0200

    Add newton's method

---
+++ Project2/Newton.m
@@ -0,0 +1,72 @@
+function [min_f, min_x, min_y, f_values] = Newton(f, x_val, y_val, e, varargin)
+    syms x y;
+    parser = inputParser;
+
+    %Υποχρεωτικά ορίσματα
+    addRequired(parser, 'f',@(g) isa(g,'function_handle') && nargin(g) == 2); %Έλεγχος f για 2 μεταβλητές
+    addRequired(parser, 'x_val', @isnumeric);
+    addRequired(parser, 'y_val', @isnumeric);
+    addRequired(parser, 'e', @(val) isnumeric(val) && val>0); %Έλεγχος ε για θετικό αριθμό
+    
+    %Προεραιτικά ορίσματα
+    addOptional(parser, 'gamma', [], @(val) isempty(val) || isnumeric(val));
+    addOptional(parser, 'armijo', false, @(val) islogical(val));
+
+    parse(parser, f, x_val, y_val, e, varargin{:});
+    params = parser.Results;
+
+    gamma = params.gamma;
+    useArmijo = params.armijo; 
+
+    a = 1e-4; %Armijo a
+    b = 0.2; %Armijo b
+    s = 1; %Αρχικό βήμα
+    
+    f_sym = f(x,y);
+    grad_f_sym = gradient(f_sym);
+    hessian_f_sym = hessian(f_sym);
+
+    grad_f = matlabFunction(grad_f_sym);
+    hessian_f = matlabFunction(hessian_f_sym);
+
+    grad = grad_f(x_val,y_val);
+    hess = hessian_f(x_val, y_val);
+    hess = hess + 1.2*eye(2); %Αρνητικα ορισμενος hessian οποτε προσθετουμε κI
+    f_values = [f(x_val,y_val)];
+ 
+    while norm(grad) > e
+        dk = -(inv(hess))*grad;
+
+        if useArmijo && isempty(gamma) %Μέθοδος armijo αφού γ δεν παρέχεται και useArmijo=true
+            m_k = 0; %Ξεκινάμε με τον μικρότερο μη αρνητικό ακέραιο
+            gamma_arm = s * b^(m_k); %Armijo rule to find gamma
+            fxk = f(x_val, y_val);
+            grad_dot_dk = dk' * grad;
+            %Επαναληπτικός κανόνας μέχρι να βρεθεί σωστό m_k
+            while fxk - f(x_val + gamma_arm*dk(1), y_val + gamma_arm*dk(2) ) <= - a*gamma_arm*grad_dot_dk
+                m_k = m_k + 1;
+                gamma_arm = s * b^m_k;
+            end
+            x_val = x_val + gamma_arm * dk(1);
+            y_val = y_val + gamma_arm * dk(2);
+
+        elseif ~useArmijo && isempty(gamma) %Μέθοδος ελαχιστοποίησης αφού γ δεν παρέχεται και useArmijo=false
+            line_search_f = @(gamma) f(x_val+gamma*dk(1), y_val+gamma*dk(2));
+            gamma_min = fminbnd(line_search_f, 0,1);
+            x_val = x_val + gamma_min * dk(1);
+            y_val = y_val + gamma_min * dk(2);
+
+        else %Μέθοδος σταθερού γ
+            x_val = x_val + gamma * dk(1);
+            y_val = y_val + gamma * dk(2);
+        end 
+        grad = grad_f(x_val,y_val);
+        hess = hessian_f(x_val, y_val) + 5*eye(2);
+        f_values = [f_values, f(x_val,y_val)];
+    end
+    min_f = f(x_val,y_val);
+    min_x = x_val;
+    min_y = y_val;
+
+end
+
--- Project2/Thema3.m
+++ Project2/Thema3.m
@@ -8,23 +8,14 @@ epsilon = 0.01;
 %Για τον κανόνα του Armijo έχουν επιλεχτεί 
 % a = 1e-4, b = 0.2, s=1
 
-
-%Σημείο i --> (0,0) Ο αλγόριθμος δεν αρχίζει καν αφού το grad στο 0 είναι
-%0<ε. Για αυτό δεν υπάρχει κάποιο γράφημα.
-[min_f_A, min_x_A, min_y_A, f_values_A] = SteepestDescent(f, 0, 0, epsilon, gamma);
-[min_f_B, min_x_B, min_y_B, f_values_B] = SteepestDescent(f, 0, 0, epsilon);
-[min_f_C, min_x_C, min_y_C, f_values_C] = SteepestDescent(f, 0, 0, epsilon, 'armijo', true);
-
-
 %Σημείο ii --> (-1,1). Βρίσκει επιτυχώς το ολικό ελάχιστο -0.81. 
-%Ο Armijo χρειάζεται περισσότερες επαναλήψεις από την μέθοδο με την
-%ελαχιστοποίηση. Κάτι τέτοιο είναι αναμενόμενο, αφού ο armijo επιλέγει
-%ένα βήμα γ που απλώς διασφαλίζει ότι η συνάρτηση μειώνεται συνεχώς(χωρίς
-% αυτό να είναι απαραίτητα το βέλτιστο γ). Τις περισσότερες επαναλήψεις
-% χρειάζεται η μέθοδος σταθερού βήματος γ.
-[min_f_A, min_x_A, min_y_A, f_values_A] = SteepestDescent(f, -1, 1, epsilon, gamma);
-[min_f_B, min_x_B, min_y_B, f_values_B] = SteepestDescent(f, -1, 1, epsilon);
-[min_f_C, min_x_C, min_y_C, f_values_C] = SteepestDescent(f, -1, 1, epsilon, 'armijo', true);
+%Armijo θέλει μια παραπάνω επανάληψη από την ελαχιστοποίηση.
+%Επηρεάζεται από το τιμή θα βάλουμε στο κ (hessian + kI)
+%Τις περισσότερες επαναλήψεις
+%χρειάζεται η μέθοδος σταθερού βήματος γ.
+[min_f_A, min_x_A, min_y_A, f_values_A] = Newton(f, -1, 1, epsilon, gamma);
+[min_f_B, min_x_B, min_y_B, f_values_B] = Newton(f, -1, 1, epsilon);
+[min_f_C, min_x_C, min_y_C, f_values_C] = Newton(f, -1, 1, epsilon, 'armijo', true);
 figure(1);
 plot(1:length(f_values_A), f_values_A, "red");
 grid on;
@@ -42,9 +33,9 @@ ylabel('f(x,y) values');
 %προσεγγίζει το 0, οπότε πληρείται η συνθήκη τερματισμού του αλγορίθμου.
 %Ελαχιστοποίση και Armijo ταυτόσημες. Τις περισσότερες επαναλήψεις
 %χρειάζεται η μέθοδος σταθερού βήματος γ.
-[min_f_A, min_x_A, min_y_A, f_values_A] = SteepestDescent(f, 1, -1, epsilon, gamma);
-[min_f_B, min_x_B, min_y_B, f_values_B] = SteepestDescent(f, 1, -1, epsilon);
-[min_f_C, min_x_C, min_y_C, f_values_C] = SteepestDescent(f, 1, -1, epsilon, 'armijo', true);
+[min_f_A, min_x_A, min_y_A, f_values_A] = Newton(f, 1, -1, epsilon, gamma);
+[min_f_B, min_x_B, min_y_B, f_values_B] = Newton(f, 1, -1, epsilon);
+[min_f_C, min_x_C, min_y_C, f_values_C] = Newton(f, 1, -1, epsilon, 'armijo', true);
 figure(2);
 plot(1:length(f_values_A), f_values_A, "red");
 grid on;
