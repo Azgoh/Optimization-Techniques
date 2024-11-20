e62b4b0e76251a64a966cd1fa5f48790a9dfcc49
NikolasTsm <ntsamitr@ece.auth.gr>
2024-11-20 19:01:34 +0200

    Correct Newton's method

--- Project2/Newton.m
+++ Project2/Newton.m
@@ -31,12 +31,10 @@ function [min_f, min_x, min_y, f_values] = Newton(f, x_val, y_val, e, varargin)
 
     grad = grad_f(x_val,y_val);
     hess = hessian_f(x_val, y_val);
-    hess = hess + 1.2*eye(2); %Αρνητικα ορισμενος hessian οποτε προσθετουμε damping κI
     f_values = [f(x_val,y_val)];
  
     while norm(grad) > e
         dk = -(inv(hess))*grad;
-
         if useArmijo && isempty(gamma) %Μέθοδος armijo αφού γ δεν παρέχεται και useArmijo=true
             m_k = 0; %Ξεκινάμε με τον μικρότερο μη αρνητικό ακέραιο
             gamma_arm = s * b^(m_k); %Armijo rule to find gamma
@@ -52,7 +50,7 @@ function [min_f, min_x, min_y, f_values] = Newton(f, x_val, y_val, e, varargin)
 
         elseif ~useArmijo && isempty(gamma) %Μέθοδος ελαχιστοποίησης αφού γ δεν παρέχεται και useArmijo=false
             line_search_f = @(gamma) f(x_val+gamma*dk(1), y_val+gamma*dk(2));
-            gamma_min = XrysosTomeas(line_search_f, 0, 10, 0.001, 0.618);
+            gamma_min = XrysosTomeas(line_search_f, 0, 10, 0.001, 0.618)
             x_val = x_val + gamma_min * dk(1);
             y_val = y_val + gamma_min * dk(2);
 
@@ -61,7 +59,7 @@ function [min_f, min_x, min_y, f_values] = Newton(f, x_val, y_val, e, varargin)
             y_val = y_val + gamma * dk(2);
         end 
         grad = grad_f(x_val,y_val);
-        hess = hessian_f(x_val, y_val) + 1.2*eye(2);
+        hess = hessian_f(x_val, y_val);
         f_values = [f_values, f(x_val,y_val)];
     end
     min_f = f(x_val,y_val);
--- Project2/Thema3.m
+++ Project2/Thema3.m
@@ -10,34 +10,41 @@ epsilon = 0.01;
 
 %Σημείο i --> (0,0) Ο αλγόριθμος δεν αρχίζει καν αφού το grad στο 0 είναι
 %0<ε. Για αυτό δεν υπάρχει κάποιο γράφημα.
-[min_f_A, min_x_A, min_y_A, f_values_A] = SteepestDescent(f, 0, 0, epsilon, gamma);
-[min_f_B, min_x_B, min_y_B, f_values_B] = SteepestDescent(f, 0, 0, epsilon);
-[min_f_C, min_x_C, min_y_C, f_values_C] = SteepestDescent(f, 0, 0, epsilon, 'armijo', true);
+[min_f_A, min_x_A, min_y_A, f_values_A] = Newton(f, 0, 0, epsilon, gamma);
+[min_f_B, min_x_B, min_y_B, f_values_B] = Newton(f, 0, 0, epsilon);
+[min_f_C, min_x_C, min_y_C, f_values_C] = Newton(f, 0, 0, epsilon, 'armijo', true);
 
-%Σημείο ii --> (-1,1). Βρίσκει επιτυχώς το ολικό ελάχιστο -0.81. 
-%Η ελαχιστοποίηση θέλει τις λιγότερες επαναλήψεις(5) και μετά ο armijo(6).
-%Τις περισσότερες επαναλήψεις χρειάζεται η μέθοδος σταθερού βήματος γ(79).
+%Σημείο ii --> (-1,1). Καταλήγουμε στο 0 το οποίο είναι τοπικό ελάχιστο και
+%πληρεί την συνθήκη τερματισμού. Με το σταθερό γ έχουμε τις λιγότερες
+%επαναλήψεις(40reps). Με την μέθοδο της ελαχιστοποίησης έχουμε 11752
+%επαναλήψεις. Κάτι τέτοιο συμβαίνει διότι ο hessian είναι αρνητικά
+%ορισμένος και έτσι η διαδικασία βέλτιστης εύρεσης γ γίνεται πολύ
+%συντηρητική με γ = 3.3083e-04. Η μέθοδος με τον κανόνα Armijo δεν συγκλίνει,
+%δεν μπορεί να βρει τιμή του γ που να μειώνει την τιμή της f. Για αυτό το 
+%σχετικό κομμάτι υπάρχει μόνο σε σχόλιο.
 [min_f_A, min_x_A, min_y_A, f_values_A] = Newton(f, -1, 1, epsilon, gamma);
 [min_f_B, min_x_B, min_y_B, f_values_B] = Newton(f, -1, 1, epsilon);
-[min_f_C, min_x_C, min_y_C, f_values_C] = Newton(f, -1, 1, epsilon, 'armijo', true);
+% [min_f_C, min_x_C, min_y_C, f_values_C] = Newton(f, -1, 1, epsilon, 'armijo', true);
 figure(1);
 plot(1:length(f_values_A), f_values_A, "red");
 grid on;
 hold on;
 plot(1:length(f_values_B), f_values_B, "blue");
-plot(1:length(f_values_C), f_values_C, "green");
+% plot(1:length(f_values_C), f_values_C, "green");
 legend('gamma = 0.1', 'gamma that minimizes $$f(x_k - \gamma_k \nabla f(x_k)$$', 'Armijo', 'interpreter', 'latex');
 title('f(x,y) values with respect to the number of repetitions. Starting point is (-1,1)'); 
 xlabel('Repetitions');
 ylabel('f(x,y) values');
 
 
-%Σημείο iii --> (1,-1). Για armijo(2reps) και σταθερό γ(171reps)
-%καταλήγει στο 0, το οποίο είναι τοπικό ελάχιστο. Δεν βρίσκει το ολικό 
-%ελάχιστο γιατί το grad της f κοντά στο 0,
-%προσεγγίζει το 0, οπότε πληρείται η συνθήκη τερματισμού του αλγορίθμου.
-%Με την ελαχιστοποίηση όμως βρίσκουμε το πραγματικό ελάχιστο της συνάρτησης
-%σε μόλις 5 επαναλήψεις.
+%Σημείο iii --> (1,-1). Για armijo(2reps), ελαχιστοποίηση(2reps), σταθερό 
+%γ(40reps) καταλήγει στο 0, το οποίο είναι τοπικό ελάχιστο. Δεν βρίσκει το 
+%ολικό ελάχιστο(ούτε το μέγιστο όπως περιμένουμε να κάνει ο newton με 
+%αρνητικά ορισμένο hessian) γιατί το grad της f κοντά στο 0, προσεγγίζει το
+%0, οπότε πληρείται η συνθήκη τερματισμού του αλγορίθμου. Επίσης, παρά τον 
+%αρνητικό hessian, επειδή αυτός έχει πολύ μικρές αρνητικές
+%ιδιοτιμές(κατ'απόλυτο), καθώς και επειδή το αρχικό σημείο βρίσκεται σε
+%κοντινή περιοχή με το τοπικό ελάχιστο η τιμή της συνάρτησης μειώνεται.
 [min_f_A, min_x_A, min_y_A, f_values_A] = Newton(f, 1, -1, epsilon, gamma);
 [min_f_B, min_x_B, min_y_B, f_values_B] = Newton(f, 1, -1, epsilon);
 [min_f_C, min_x_C, min_y_C, f_values_C] = Newton(f, 1, -1, epsilon, 'armijo', true);
