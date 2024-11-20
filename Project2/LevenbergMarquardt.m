7b5062da4a0f8f5f60bf8d9039544aa96c6d5edd
NikolasTsm <ntsamitr@ece.auth.gr>
2024-11-20 19:26:19 +0200

    Add Levenberg Marquardt

--- Project2/LevenbergMarquardt.m
+++ Project2/LevenbergMarquardt.m
@@ -1,7 +1,70 @@
-function [outputArg1,outputArg2] = LevenbergMarquardt(inputArg1,inputArg2)
-%LEVENBERGMARQUARDT Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+function [min_f, min_x, min_y, f_values] = Newton(f, x_val, y_val, e, varargin)
+    syms x y;
+    parser = inputParser;
 
+    %Υποχρεωτικά ορίσματα
+    addRequired(parser, 'f',@(g) isa(g,'function_handle') && nargin(g) == 2); %Έλεγχος f για 2 μεταβλητές
+    addRequired(parser, 'x_val', @isnumeric);
+    addRequired(parser, 'y_val', @isnumeric);
+    addRequired(parser, 'e', @(val) isnumeric(val) && val>0); %Έλεγχος ε για θετικό αριθμό
+    
+    %Προεραιτικά ορίσματα
+    addOptional(parser, 'gamma', [], @(val) isempty(val) || isnumeric(val));
+    addOptional(parser, 'armijo', false, @(val) islogical(val));
+
+    parse(parser, f, x_val, y_val, e, varargin{:});
+    params = parser.Results;
+
+    gamma = params.gamma;
+    useArmijo = params.armijo; 
+
+    a = 1e-4; %Armijo a
+    b = 0.2; %Armijo b
+    s = 100; %Αρχικό βήμα
+    
+    f_sym = f(x,y);
+    grad_f_sym = gradient(f_sym);
+    hessian_f_sym = hessian(f_sym);
+
+    grad_f = matlabFunction(grad_f_sym);
+    hessian_f = matlabFunction(hessian_f_sym);
+
+    grad = grad_f(x_val,y_val);
+    hess = hessian_f(x_val, y_val);
+    f_values = [f(x_val,y_val)];
+ 
+    while norm(grad) > e
+        hess = hess + 1.5*eye(2);
+        dk = -(inv(hess))*grad;
+        if useArmijo && isempty(gamma) %Μέθοδος armijo αφού γ δεν παρέχεται και useArmijo=true
+            m_k = 0; %Ξεκινάμε με τον μικρότερο μη αρνητικό ακέραιο
+            gamma_arm = s * b^(m_k); %Armijo rule to find gamma
+            fxk = f(x_val, y_val);
+            grad_dot_dk = dk' * grad;
+            %Επαναληπτικός κανόνας μέχρι να βρεθεί σωστό m_k
+            while fxk - f(x_val + gamma_arm*dk(1), y_val + gamma_arm*dk(2) ) <= - a*gamma_arm*grad_dot_dk
+                m_k = m_k + 1;
+                gamma_arm = s * b^m_k;
+            end
+            x_val = x_val + gamma_arm * dk(1);
+            y_val = y_val + gamma_arm * dk(2);
+
+        elseif ~useArmijo && isempty(gamma) %Μέθοδος ελαχιστοποίησης αφού γ δεν παρέχεται και useArmijo=false
+            line_search_f = @(gamma) f(x_val+gamma*dk(1), y_val+gamma*dk(2));
+            gamma_min = XrysosTomeas(line_search_f, 0, 10, 0.001, 0.618);
+            x_val = x_val + gamma_min * dk(1);
+            y_val = y_val + gamma_min * dk(2);
+
+        else %Μέθοδος σταθερού γ
+            x_val = x_val + gamma * dk(1);
+            y_val = y_val + gamma * dk(2);
+        end 
+        grad = grad_f(x_val,y_val);
+        hess = hessian_f(x_val, y_val);
+        f_values = [f_values, f(x_val,y_val)];
+    end
+    min_f = f(x_val,y_val);
+    min_x = x_val;
+    min_y = y_val;
+
+end
---
+++ Project2/Thema4.m
@@ -0,0 +1,52 @@
+clc, clearvars, clear
+
+f = @(x,y)x.^5 .* exp(-x.^2 - y.^2);
+
+gamma = 0.1; %Σταθερό γ για το α υποερώτημα. Μικρό γ για να φαίνεται φανερά η διαφορά με τις άλλες μεθόδους
+epsilon = 0.01;
+
+%Για τον κανόνα του Armijo έχουν επιλεχτεί 
+% a = 1e-4, b = 0.2, s=100
+
+%O hessian ΔΕΝ είναι θετικά ορισμένος. Προσθέτω 1.5I στον hessian.
+
+%Σημείο i --> (0,0) Ο αλγόριθμος δεν αρχίζει καν αφού το grad στο 0 είναι
+%0<ε. Για αυτό δεν υπάρχει κάποιο γράφημα.
+[min_f_A, min_x_A, min_y_A, f_values_A] = LevenbergMarquardt(f, 0, 0, epsilon, gamma);
+[min_f_B, min_x_B, min_y_B, f_values_B] = LevenbergMarquardt(f, 0, 0, epsilon);
+[min_f_C, min_x_C, min_y_C, f_values_C] = LevenbergMarquardt(f, 0, 0, epsilon, 'armijo', true);
+
+%Σημείο ii --> (-1,1). Και οι 3 καταλήγουν στο ολικό ελάχιστο -0.81. Με το
+%σταθερό γ χρειάζεται 91 επαναλήψεις. Με την ελαχιστοποίηση 5. Με τον
+%armijo 10.
+[min_f_A, min_x_A, min_y_A, f_values_A] = LevenbergMarquardt(f, -1, 1, epsilon, gamma);
+[min_f_B, min_x_B, min_y_B, f_values_B] = LevenbergMarquardt(f, -1, 1, epsilon);
+[min_f_C, min_x_C, min_y_C, f_values_C] = LevenbergMarquardt(f, -1, 1, epsilon, 'armijo', true);
+figure(1);
+plot(1:length(f_values_A), f_values_A, "red");
+grid on;
+hold on;
+plot(1:length(f_values_B), f_values_B, "blue");
+plot(1:length(f_values_C), f_values_C, "green");
+legend('gamma = 0.1', 'gamma that minimizes $$f(x_k - \gamma_k \nabla f(x_k)$$', 'Armijo', 'interpreter', 'latex');
+title('f(x,y) values with respect to the number of repetitions. Starting point is (-1,1)'); 
+xlabel('Repetitions');
+ylabel('f(x,y) values');
+
+
+%Σημείο iii --> (1,-1). O armijo καταλήγει στο 0 σε 2 επαναλήψεις. Με
+%σταθερό γ καταλήγει στο 0 σε 207 επαναλήωεις. Με την ελαχιστοποίηση
+%καταλήγουμε στο ολικό ελάχιστο -0.81 σε 6 επαναλήψεις. 
+[min_f_A, min_x_A, min_y_A, f_values_A] = LevenbergMarquardt(f, 1, -1, epsilon, gamma);
+[min_f_B, min_x_B, min_y_B, f_values_B] = LevenbergMarquardt(f, 1, -1, epsilon);
+[min_f_C, min_x_C, min_y_C, f_values_C] = LevenbergMarquardt(f, 1, -1, epsilon, 'armijo', true);
+figure(2);
+plot(1:length(f_values_A), f_values_A, "red");
+grid on;
+hold on;
+plot(1:length(f_values_B), f_values_B, "blue");
+plot(1:length(f_values_C), f_values_C, "green");
+legend('gamma = 0.1', 'gamma that minimizes $$f(x_k - \gamma_k \nabla f(x_k)$$', 'Armijo', 'interpreter', 'latex');
+title('f(x,y) values with respect to the number of repetitions. Starting point is (1,-1)'); 
+xlabel('Repetitions');
+ylabel('f(x,y) values');
